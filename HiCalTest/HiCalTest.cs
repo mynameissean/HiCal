d2646f1eb7530b90baa779eb35555894d7d16a6d
Sean Davis <aaaathatisfouras@gmail.com>
2021-03-16 21:26:30 -0400

    Added some new simple tests

--- HiCalTest/HiCalTest.cs
+++ HiCalTest/HiCalTest.cs
@@ -12,13 +12,34 @@ namespace HiCalTest
         public void GivenTestSetup()
         {
             GivenTest(new HiCalBrute());
-            GivenTest(new HiCalMatch());
+            //GivenTest(new HiCalMatch());
         }
 
         private void GivenTest(HiCalBase MeetingMerger)
         {
             List<Meeting> meetingsToCheck = new List<Meeting> { new Meeting(0, 1), new Meeting(3, 5), new Meeting(4, 8), new Meeting(10, 12), new Meeting(9, 10) };
             List<Meeting> expectedOutput = new List<Meeting> { new Meeting(0, 1), new Meeting(3, 8), new Meeting(9, 12) };
+            Assert.AreEqual(MeetingMerger.ToString(MeetingMerger.MergeRanges(meetingsToCheck)), MeetingMerger.ToString(expectedOutput));
+        }
+
+        [TestMethod]
+        public void SimpleTestSetup()
+        {
+            SimpleTest(new HiCalBrute());
+            //SimpleTestSetup(new HiCalMatch());
+        }
+
+        private void SimpleTest(HiCalBase MeetingMerger)
+        {
+            List<Meeting> meetingsToCheck = new List<Meeting> { new Meeting(0, 1), new Meeting(1, 2), new Meeting(2, 3), new Meeting(3, 4), new Meeting(4, 5) };
+            List<Meeting> expectedOutput = new List<Meeting> { new Meeting(0, 5) };
+            Assert.AreEqual(MeetingMerger.ToString(MeetingMerger.MergeRanges(meetingsToCheck)), MeetingMerger.ToString(expectedOutput));
+            meetingsToCheck = new List<Meeting> {  new Meeting(4, 5), new Meeting(3, 4), new Meeting(2, 3), new Meeting(1, 2), new Meeting(0, 1) };
+            Assert.AreEqual(MeetingMerger.ToString(MeetingMerger.MergeRanges(meetingsToCheck)), MeetingMerger.ToString(expectedOutput));
+
+            //Now with spaces in between
+            meetingsToCheck = new List<Meeting> { new Meeting(0, 1), new Meeting(2, 3), new Meeting(4, 5), new Meeting(6, 7), new Meeting(8, 9) };
+            expectedOutput = new List<Meeting> { new Meeting(0, 1), new Meeting(2, 3), new Meeting(4, 5), new Meeting(6, 7), new Meeting(8, 9) };
             Assert.AreEqual(MeetingMerger.ToString(MeetingMerger.MergeRanges(meetingsToCheck)), MeetingMerger.ToString(expectedOutput));
         }
 
